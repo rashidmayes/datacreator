bf342e749d9c3bf017af7ddd99eb2cdf619c79ba
Rashid Mayes <rashid@astrientlabs.com>
2020-07-31 12:38:16 -0400

    Update package

--- src/main/java/com/aerospike/pub/dc/keygen/KeyGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.aerospike.pub.dc.keygen;
-
-import com.aerospike.client.Key;
-
-public interface KeyGenerator {
-	public Key generate(String name, int keyLength, String namespace, String set);
-}
